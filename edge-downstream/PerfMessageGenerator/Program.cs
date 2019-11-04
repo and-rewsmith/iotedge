b5790ba015b81542bc6be646ce4b9aaf28e9f1c7
Andrew <andsmi@microsoft.com>
2019-11-04 14:46:16 -0800

    downstream device

---
+++ edge-downstream/PerfMessageGenerator/Program.cs
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace PerfMessageGenerator
+{
+    using System;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Edge.ModuleUtil;
+    using Microsoft.Azure.Devices.Edge.Util;
+    using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
+
+    class Program
+    {
+        static readonly ILogger Logger = ModuleUtil.CreateLogger("PerfMessageGenerator");
+
+        public static int Main() => MainAsync().Result;
+
+        static async Task<int> MainAsync()
+        {
+            Logger.LogInformation($"Starting PerfMessageGenerator with the following settings:\r\n{Settings.Current}");
+
+            (CancellationTokenSource cts, ManualResetEventSlim completed, Option<object> handler) = ShutdownHandler.Init(TimeSpan.FromSeconds(5), Logger);
+
+            await GenerateMessagesAsync(cts);
+
+            completed.Set();
+            handler.ForEach(h => GC.KeepAlive(h));
+            Logger.LogInformation("PerfMessageGenerator Main() finished.");
+            await Task.Delay(1000 * 60 * 5);
+            return 0;
+        }
+
+        static async Task GenerateMessagesAsync(CancellationTokenSource cts)
+        {
+
+            long messageIdCounter = 0;
+            string batchId = Guid.NewGuid().ToString();
+            var messageBody = new byte[Settings.Current.MessageSizeInBytes];
+            byte[] payload = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageBody));
+
+            while (!cts.Token.IsCancellationRequested)
+            {
+                if (messageIdCounter % 1000 == 0)
+                {
+                    Logger.LogInformation($"{batchId}: Sent {messageIdCounter} messages");
+                }
+
+                var message = new Message(payload);
+                message.Properties.Add("sequenceNumber", messageIdCounter.ToString());
+                message.Properties.Add("batchId", batchId);
+
+                try
+                {
+                    await moduleClient.SendEventAsync(Settings.Current.OutputName, message);
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError($"[GenerateMessagesAsync] Sequence number {messageIdCounter}, BatchId: {batchId};{Environment.NewLine}{e}");
+                }
+
+                messageIdCounter += 1;
+            }
+        }
+    }
+}
---
+++ edge-downstream/PerfMessageGenerator/Settings.cs
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace PerfMessageGenerator
+{
+    using System;
+    using System.IO;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Extensions.Configuration;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Newtonsoft.Json.Serialization;
+
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class Settings
+    {
+        static readonly Lazy<Settings> DefaultSettings = new Lazy<Settings>(
+            () =>
+            {
+                IConfiguration configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("config/settings.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+
+                return new Settings(
+                    configuration.GetValue<ulong>("messageSizeInBytes", 1024),
+                    configuration.GetValue<TransportType>("transportType", TransportType.Amqp_Tcp_Only),
+                    configuration.GetValue<string>("outputName", "output1"));
+            });
+
+        Settings(
+            ulong messageSizeInBytes,
+            TransportType transportType,
+            string outputName)
+        {
+            this.MessageSizeInBytes = messageSizeInBytes;
+            this.TransportType = transportType;
+            this.OutputName = outputName;
+        }
+
+        public static Settings Current => DefaultSettings.Value;
+        public ulong MessageSizeInBytes { get; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TransportType TransportType { get; }
+
+        public string OutputName { get; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
