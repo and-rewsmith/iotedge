93cb32ead0988cd129da1b2f7706689e900cc0f1
Andrew Smith <als5ev@virginia.edu>
2020-11-16 22:52:41 +0000

    clean up cargo toml

--- test/modules/GenericMqttTester/src/main.rs
+++ test/modules/GenericMqttTester/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tokio;
+use tracing::{info, subscriber, Level};
+use tracing_subscriber::fmt::Subscriber;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    init_logging();
+
+    info!("Starting generic mqtt test module");
+
+    Ok(())
+}
+
+fn init_logging() {
+    let subscriber = Subscriber::builder().with_max_level(Level::TRACE).finish();
+    let _ = subscriber::set_global_default(subscriber);
 }
