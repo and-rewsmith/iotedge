25ac729c02e6d4dc07840ac9d5f92acf18384a2e
Andrew Smith <als5ev@virginia.edu>
2020-11-21 02:14:30 +0000

    notes

--- test/modules/GenericMqttTester/src/main.rs
+++ test/modules/GenericMqttTester/src/main.rs
@@ -6,11 +6,23 @@ use tracing_subscriber::fmt::Subscriber;
 
 use mqtt3::{
     proto::{QoS, SubscribeTo},
-    Client,
+    Client, Event,
 };
 
 use mqtt_broker_tests_util::client;
 use mqtt_util::client_io::ClientIoSource;
+
+// TODO;
+/*
+add configuration for whether we are sending or receiving
+
+if we are sending then:
+- spawn task to send that stores messages in state and waits for expiry
+- how to determine expiry?
+
+if we are receiving:
+- when receive a publication then send one back to broker
+*/
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -28,7 +40,6 @@ async fn main() -> Result<()> {
         .unwrap();
 
     info!("polling client");
-
     poll_client(client).await;
 
     Ok(())
@@ -36,7 +47,22 @@ async fn main() -> Result<()> {
 
 async fn poll_client(mut client: Client<ClientIoSource>) {
     while let Ok(Some(event)) = client.try_next().await {
-        info!("received event {:?}", event)
+        info!("received event {:?}", event);
+
+        match event {
+            Event::NewConnection { .. } => {
+                info!("received new connection");
+            }
+            Event::Publication(publication) => {
+                info!("received publication");
+            }
+            Event::SubscriptionUpdates(_) => {
+                info!("received subscription update");
+            }
+            Event::Disconnected(_) => {
+                info!("received disconnect");
+            }
+        };
     }
 
     error!("stopped polling client");
